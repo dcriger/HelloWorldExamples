af37556d113fe5d010578dd5440122b45b463905
Dustin W Criger <dcriger@ecc-jaguar.csuchico.edu>
2017-02-25 17:39:00 -0800

    added main.java file and edited README.md to include languages used, fixes #1, fixes #2

---
+++ main.java
@@ -0,0 +1,10 @@
+//main.java
+
+//Example courtesy of http://introcs.cs.princeton.edu/java/11hello/
+
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
+}
